0983e437ad0fb83ba74b937ef78d9df6bdf9bc5a
Dexterdaniel <dexterdaniel@gmail.com>
2021-11-03 21:17:31 +0100

    Swagger API Documentation Added

--- backend_api/api_core/urls.py
+++ backend_api/api_core/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from rest_framework import permissions
 from authentication import urls as authn
 from . import urls_apidocs
 
